7d8f4b062107a95e904b45e07efd4e68a370c58d
Siddh8051 <siddharthkumar805100@gmail.com>
2024-08-09 23:31:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     API_ID = int(os.environ.get("API_ID", "29029800"))
     API_HASH = os.environ.get("API_HASH", "0258b310e9ebb9b5155b5740d6ecf126")
-    AUTH_USERS = os.environ.get("AUTH_USERS", "6790518589")
+    AUTH_USERS = os.environ.get("AUTH_USERS", "6790518589""1957816775""6899180574")
     PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
