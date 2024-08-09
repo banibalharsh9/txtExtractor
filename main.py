26dbb188479a1b499ec9a77e941c9fefdd1317bd
Siddh8051 <siddharthkumar805100@gmail.com>
2024-08-09 21:31:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ prefixes = ["/", "~", "?", "!"]
 plugins = dict(root="plugins")
 if __name__ == "__main__" :
     bot = Client(
-        "StarkBot",
+        "macc",
         bot_token=Config.BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
